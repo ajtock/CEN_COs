eb47d1a43472590bcf96d80f8f8eefca8c7f48b3
ajtock <andytock@gmail.com>
2021-09-13 17:54:53 +0100

    CTL3.9

---
+++ plots/browser_tracks_plots_rename.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mv CTL3.9_browser_tracks_Chr3-9749802-9799635.pdf CTL3.9_browser_tracks_Chr3-9749802-9799635_HS13.pdf
+mv CTL3.9_browser_tracks_Chr3-9799634-9851690.pdf CTL3.9_browser_tracks_Chr3-9799634-9851690_HS14.pdf
+mv CTL3.9_browser_tracks_Chr3-9903199-9958690.pdf CTL3.9_browser_tracks_Chr3-9903199-9958690_HS15.pdf
+mv CTL3.9_browser_tracks_Chr3-9958689-10010239.pdf CTL3.9_browser_tracks_Chr3-9958689-10010239_HS16.pdf
+mv CTL3.9_browser_tracks_Chr3-10010238-10064107.pdf CTL3.9_browser_tracks_Chr3-10010238-10064107_HS17.pdf
+mv CTL3.9_browser_tracks_Chr3-10149801-10197599.pdf CTL3.9_browser_tracks_Chr3-10149801-10197599_HS18.pdf
+mv CTL3.9_browser_tracks_Chr3-10197598-10232263.pdf CTL3.9_browser_tracks_Chr3-10197598-10232263_HS19.pdf
+mv CTL3.9_browser_tracks_Chr3-10331821-10378940.pdf CTL3.9_browser_tracks_Chr3-10331821-10378940_HS1.pdf
+mv CTL3.9_browser_tracks_Chr3-10715518-10767880.pdf CTL3.9_browser_tracks_Chr3-10715518-10767880_HS2.pdf
+mv CTL3.9_browser_tracks_Chr3-10949217-10990951.pdf CTL3.9_browser_tracks_Chr3-10949217-10990951_HS3.pdf
+mv CTL3.9_browser_tracks_Chr3-11111160-11138820.pdf CTL3.9_browser_tracks_Chr3-11111160-11138820_HS11.pdf
+mv CTL3.9_browser_tracks_Chr3-11138819-11165014.pdf CTL3.9_browser_tracks_Chr3-11138819-11165014_HS4.pdf
+mv CTL3.9_browser_tracks_Chr3-11242243-11299965.pdf CTL3.9_browser_tracks_Chr3-11242243-11299965_HS5.pdf
+mv CTL3.9_browser_tracks_Chr3-11728734-11754887.pdf CTL3.9_browser_tracks_Chr3-11728734-11754887_HS6.pdf
+mv CTL3.9_browser_tracks_Chr3-11791684-11848487.pdf CTL3.9_browser_tracks_Chr3-11791684-11848487_HS12.pdf
+mv CTL3.9_browser_tracks_Chr3-12827459-13403988.pdf CTL3.9_browser_tracks_Chr3-12827459-13403988_CS1.pdf
+mv CTL3.9_browser_tracks_Chr3-13582542-16205121.pdf CTL3.9_browser_tracks_Chr3-13582542-16205121_CS2.pdf
+mv CTL3.9_browser_tracks_Chr3-16205120-16482813.pdf CTL3.9_browser_tracks_Chr3-16205120-16482813_CS3.pdf
+mv CTL3.9_browser_tracks_Chr3-16579472-16977775.pdf CTL3.9_browser_tracks_Chr3-16579472-16977775_CS4.pdf
+mv CTL3.9_browser_tracks_Chr3-17184653-17210731.pdf CTL3.9_browser_tracks_Chr3-17184653-17210731_HS7.pdf
+mv CTL3.9_browser_tracks_Chr3-17935725-17981724.pdf CTL3.9_browser_tracks_Chr3-17935725-17981724_HS8.pdf
+mv CTL3.9_browser_tracks_Chr3-18143929-18202582.pdf CTL3.9_browser_tracks_Chr3-18143929-18202582_HS9.pdf
+mv CTL3.9_browser_tracks_Chr3-18202581-18255619.pdf CTL3.9_browser_tracks_Chr3-18202581-18255619_HS10.pdf
